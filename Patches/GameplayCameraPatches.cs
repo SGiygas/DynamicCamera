7313eed40fb48be563539521293797f933b94494
SGiygas <bonniesbumpercars@gmail.com>
2023-11-21 18:01:05 +0100

    Initial commit

---
+++ Patches/GameplayCameraPatches.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace DynamicCamera.Patches;
+
+[HarmonyPatch(typeof(Reptile.GameplayCamera), "Awake")]
+public class GameplayCameraAwakePatch
+{
+    public static void Postfix(Camera ___cam)
+    {
+        Plugin.Instance.Initialize(___cam.fieldOfView);
+    }
+}
+
+[HarmonyPatch(typeof(Reptile.GameplayCamera), nameof(Reptile.GameplayCamera.UpdateCamera))]
+public class GameplayCameraUpdatePatch
+{
+    public static void Postfix(Reptile.GameplayCamera __instance,
+                               Camera ___cam,
+                               Transform ___realTf,
+                               Reptile.Player ___player,
+                               Reptile.CameraMode ___cameraMode,
+                               Reptile.CameraMode ___cameraModePrev,
+                               float ___cameraModeFade)
+    {
+        Plugin plugin = Plugin.Instance;
+
+        float speed = plugin.UseTotalSpeed.Value ? ___player.GetTotalSpeed() : ___player.GetForwardSpeed();
+        float speedNormalized = speed / plugin.MaxSpeed;
+        ___cam.fieldOfView = plugin.UpdateFOV(speedNormalized);
+
+        if (plugin.EnableDolly.Value)
+        {
+            Vector3 lookAtPosition;
+
+            if (___cameraModePrev != null)
+            {
+                float t = FadeOutCubic(___cameraModeFade);
+                lookAtPosition = Vector3.Lerp(___cameraModePrev.lookAtPos, ___cameraMode.lookAtPos, t);
+            }
+            else
+            {
+                lookAtPosition = ___cameraMode.lookAtPos;
+            }
+
+            // Direction from A to B = B - A
+            Vector3 viewVector = ___realTf.position - lookAtPosition;
+            float distanceToCharacter = viewVector.magnitude;
+            // Normalized view direction
+            Vector3 viewDirection = viewVector / distanceToCharacter;
+
+            Vector3 newDirection = viewDirection * plugin.UpdateDistance(distanceToCharacter);
+
+            ___realTf.position = lookAtPosition + newDirection;
+        }
+    }
+
+    private static float FadeOutCubic(float f)
+    {
+        f = 1f - f;
+        return 1.0f - f * f * f;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,94 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Reptile;
+using UnityEngine;
+
+namespace DynamicCamera;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance { get; private set; }
+
+    public float DefaultFOV { get; private set; }
+    public float MaxSpeed { get; private set; }
+
+    public ConfigEntry<bool> UseTotalSpeed { get; private set; }
+    public ConfigEntry<bool> EnableDolly { get; private set; }
+    public ConfigEntry<float> MaxFov { get; private set; }
+    public ConfigEntry<float> MaxSpeedKmh { get; private set; }
+    public ConfigEntry<float> AdjustmentSpeed { get; private set; }
+
+    private float _currentFOV;
+    private float _currentFOVT;
+    private float _fovTangent;
+
+    private readonly AnimationCurve _easeIn = new(
+        new Keyframe(0.0f, 0.0f, 0.0f, 0.0f),
+        new Keyframe(1.0f, 1.0f, 2.0f, 0.0f)
+    );
+
+    private void Awake()
+    {
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+        EnableDolly = Config.Bind("Settings", "Enable Dolly Zoom Effect", true, "Makes the FOV change while the visual position of the camera doesn't.");
+        MaxFov = Config.Bind("Settings", "Maximum FOV", 90.0f);
+        AdjustmentSpeed = Config.Bind("Settings", "Adjustment Speed", 3.5f, "How fast the FOV adjusts in degrees per second.");
+        UseTotalSpeed = Config.Bind("Settings", "Use Total Speed", true, "Whether to use the total speed or only the lateral (horizontal) speed.");
+        MaxSpeedKmh = Config.Bind("Settings", "Maximum Speed (km/h)", 60.0f);
+
+        MaxSpeed = MaxSpeedKmh.Value / 3.6f;
+
+        Instance = this;
+
+        var patches = new Harmony("sgiygas.fastCamera");
+        patches.PatchAll();
+    }
+
+    public void Initialize(float defaultFOV)
+    {
+        DefaultFOV = defaultFOV;
+        _currentFOV = defaultFOV;
+        _currentFOVT = 0.0f;
+
+        if (EnableDolly.Value)
+        {
+            _fovTangent = Mathf.Tan(defaultFOV * 0.5f * Mathf.Deg2Rad);
+        }
+    }
+
+    public float UpdateFOV(float playerSpeedNormalized)
+    {
+        float adjustmentAmount = AdjustmentSpeed.Value * Core.dt;
+
+        if (_currentFOVT < playerSpeedNormalized)
+        {
+            _currentFOVT += adjustmentAmount;
+            _currentFOVT = Mathf.Min(_currentFOVT, playerSpeedNormalized);
+        }
+        else if (_currentFOVT > playerSpeedNormalized)
+        {
+            _currentFOVT -= adjustmentAmount;
+            _currentFOVT = Mathf.Max(_currentFOVT, playerSpeedNormalized);
+        }
+
+        float t = _easeIn.Evaluate(_currentFOVT);
+        _currentFOV = Mathf.Lerp(DefaultFOV, MaxFov.Value, t);
+
+        return _currentFOV;
+    }
+
+    public float UpdateDistance(float targetDistance)
+    {
+        return GetDistanceRelativeToFOV(targetDistance, _currentFOV);
+    }
+
+    private float GetDistanceRelativeToFOV(float targetDistance, float fieldOfView)
+    {
+        float frustumHeight = 2.0f * targetDistance * _fovTangent;
+        float adjustedDistance = frustumHeight / (2.0f * Mathf.Tan(0.5f * fieldOfView * Mathf.Deg2Rad));
+        return adjustedDistance;
+    }
+}
